70332e6ba3d8cb00ca0ca793da8ab1653856ed3c
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-07-19 15:45:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(["Obi-Wan Kenobi", "Qui-Gon Jin", "Anakin Skywalker", "Asohka Tano"])
-   
-   << "Revan"
+  add_element_to_end_of_array << "Revan"
 end
 
 def add_element_to_start_of_array(array, element)
